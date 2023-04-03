7665b01ac7f4b1516f608d58181011d3a0c1ae3e
RhumeLeap <rhume.disi@turntabl.io>
2023-04-03 23:03:34 +0100

    Added the missing value (10) to the array of values in the initialize method.

--- _BlackJack/src/main/java/io/turntabl/DeckOfCard.java
+++ _BlackJack/src/main/java/io/turntabl/DeckOfCard.java
@@ -2,11 +2,11 @@ package io.turntabl;
 
 /**
  * @author Rhume Disi
- * @date April 3, 2023
+ * {@code @date} April 3, 2023
  */
 public class DeckOfCard {
 
-    private Card[] allCards;
+    private final Card[] allCards;
 
     public DeckOfCard() {
         this.allCards = new Card[52];
@@ -14,14 +14,19 @@ public class DeckOfCard {
     }
 
     private void initializeAllCards() {
-        char[] suits = {'H', 'D', 'C', 'S'};
-        char[] values = {'2', '3', '4', '5', '6', '7', '8', '9', 'J', 'Q', 'K', 'A'};
+        String[] suits = {"H", "D", "C", "S"};
+        String[] values = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
         int counter = 0;
 
-        for (char suit : suits) {
-            for (char value : values) {
+        for (String suit : suits) {
+            for (String value : values) {
                 allCards[counter++] = new Card(suit, value);
+                //counter++;
             }
         }
+    }
+
+    public Card[] getAllCards() {
+        return this.allCards;
     }
 }
