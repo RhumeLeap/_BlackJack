dc83dbfc7f69945064a44fa2fcb11e1938318b8e
RhumeLeap <rhume.disi@turntabl.io>
2023-04-03 23:01:21 +0100

    Changed the suit and value instance variable type from char to String.

--- _BlackJack/src/main/java/io/turntabl/Card.java
+++ _BlackJack/src/main/java/io/turntabl/Card.java
@@ -2,42 +2,24 @@ package io.turntabl;
 
 /**
  * @author Rhume Disi
- * @date April 3, 2023
+ * {@code @date} April 3, 2023
  */
 public final class Card {
 
-    private char suit;
-    private char value;
+    private final String suit;
+    private final String value;
 
-    public Card(char suit, char value) {
+    public Card(String suit, String value) {
         this.suit = suit;
         this.value = value;
     }
 
-    public char getSuit() {
+    public String getSuit() {
         return suit;
     }
 
-    public char getValue() {
+    public String getValue() {
         return value;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Card card = (Card) o;
-
-        if (suit != card.suit) return false;
-        return value == card.value;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = suit;
-        result = 31 * result + (int) value;
-        return result;
     }
 
     @Override
