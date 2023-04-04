42e74e5b410985854227cb4e8b8d9fd676a0f714
RhumeLeap <rhume.disi@turntabl.io>
2023-04-04 08:08:14 +0100

    Implemented method to get total value of cards in player's hand.

---
+++ _BlackJack/src/main/java/io/turntabl/player/Player.java
@@ -0,0 +1,74 @@
+package io.turntabl.player;
+
+import io.turntabl.card.Card;
+import io.turntabl.strategy.DefaultGameStrategy;
+import io.turntabl.strategy.GameStrategy;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Player {
+
+    private final String name;
+    private Set<Card> handOfCards = new HashSet<>();
+    private GameStrategy gameStrategy;
+
+    public Player(String name) {
+        this(name, new DefaultGameStrategy());
+    }
+
+    public Player(String name, GameStrategy gameStrategy) {
+        this.name = name;
+        this.gameStrategy = gameStrategy;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public GameStrategy getGameStrategy() {
+        return gameStrategy;
+    }
+
+    public void setGameStrategy(GameStrategy gameStrategy) {
+        this.gameStrategy = gameStrategy;
+    }
+
+    /** Card dealt to a player. */
+    public void receiveOneCard(Card dealtCard) {
+        handOfCards.add(dealtCard);
+    }
+
+    public Set<Card> getCardsOnHand() {
+        return handOfCards;
+    }
+
+    /** Total value of all cards in a player's hand. */
+    public int getTotalValueOfCards() {
+        int totalValue = 0;
+        for (Card eachCard : handOfCards) {
+            totalValue += eachCard.getIntegerValueOfCard();
+        }
+        return totalValue;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+
+        return name.equals(player.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "Player { name = " + name + ", handOfCards=" + handOfCards +
+                ", gameStrategy = " + gameStrategy + " }";
+    }
+}
